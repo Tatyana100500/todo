c20c6820bfeac0e34f0997df880be0e3d6138b83
Tatiana Chernishova <chernishovate@mail.ru>
2022-11-18 22:05:54 +0000

    add

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+
+import Todo from './TaskManager'
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Todo />
+    </div>
+  );
+}
+
+export default App;
